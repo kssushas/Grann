53c6f5cce71d67854fc90b4fc64bb5e79611bdbf
kssushas <ksuysha.kovtun@gmail.com>
2023-11-12 13:56:42 +0200

    cakesApi update

--- src/js/cakesApi.js
+++ src/js/cakesApi.js
@@ -1,39 +1,50 @@
-import img from '../img/best-1-1x.jpg';
-import img2 from '../img/best-1-2x-min.jpg';
+import best11 from '../img/best-1-1x.jpg';
+import best12 from '../img/best-1-2x-min.jpg';
+import best21 from '../img/best-2-1x-min.jpg';
+import best22 from '../img/best-2-2x-min.jpg';
+import best31 from '../img/best-3-1x-min.jpg';
+import best32 from '../img/best-3-2x-min.jpg';
+import best41 from '../img/catal-1-1x-min.jpg';
+import best42 from '../img/catal-1-2x-min.jpg';
+import best51 from '../img/catal-2-1x-min.jpg';
+import best52 from '../img/catal-2-2x-min.jpg';
+import best61 from '../img/catal-3-1x-min.jpg';
+import best62 from '../img/catal-3-2x-min.jpg';
+
 const responseBestSellers = [
     {
-        src : img,
-        srcset : img2,
+        src : best11,
+        srcset : best12,
         title : "Mango-Passionfruit mousse cake",
         price : "1200UAH/3kg",
     },
     {
-        src : "./img/best-2-1x.jpg",
-        srcset : "./img/best-2-2x-min.jpg 2x",
+        src : best21,
+        srcset : best22,
         title : "Chocolate brownie",
         price : "300UAH/3pcs",
     },
     {
-        src : "./img/best-3-1x-min.jpg",
-        srcset : "./img/best-3-2x-min.jpg 2x",
+        src : best31,
+        srcset : best32,
         title : "Choux pastry",
         price : "400UAH/4pcs",
     },
     {
-        src : "./img/catal-1-1x-min.jpg",
-        srcset : "./img/catal-1-2x-min.jpg 2x",
+        src : best41,
+        srcset : best42,
         title : "Сhocolate cake with raspberries",
         price : "1000UAH/2kg",
     },
     {
-        src : "./img/catal-2-1x-min.jpg",
-        srcset : "./img/catal-2-2x-min.jpg 2x",
+        src : best51,
+        srcset : best52,
         title : "Passionfruit kiwi cake",
         price : "1100UAH/1.5kg",
     },
     {
-        src : "./img/catal-3-1x-min.jpg",
-        srcset : "./img/catal-3-2x-min.jpg 2x",
+        src : best61,
+        srcset : best62,
         title : "Snickers cake",
         price : "1400UAH/3kg",
     },
