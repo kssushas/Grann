ec82ffceb7bb80a184039caf378dfd6bda941978
kssushas <ksuysha.kovtun@gmail.com>
2023-11-17 13:07:14 +0200

    swiper

---
+++ src/js/catalog.js
@@ -0,0 +1,12 @@
+new Swiper ('.swiper', {
+    
+        
+        spaceBetween: 20,
+        slidesPerView: 2,
+        // freeMode: true,
+        // breakpoints: {
+        //   1200: {
+        //     spaceBetween: 20,
+        //   }
+        // }
+})
