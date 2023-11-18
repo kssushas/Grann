3b2d919648e885ae9f367ea790ee3164921b531d
kssushas <ksuysha.kovtun@gmail.com>
2023-11-18 19:58:58 +0200

    catalog

--- src/js/cakesApi.js
+++ src/js/cakesApi.js
@@ -2,14 +2,111 @@ import best11 from '../img/best-1-1x.jpg';
 import best12 from '../img/best-1-2x-min.jpg';
 import best21 from '../img/best-2-1x-min.jpg';
 import best22 from '../img/best-2-2x-min.jpg';
-import best31 from '../img/best-3-1x-min.jpg';
-import best32 from '../img/best-3-2x-min.jpg';
+import best31 from '../img/best-min.jpg';
 import best41 from '../img/catal-1-1x-min.jpg';
 import best42 from '../img/catal-1-2x-min.jpg';
 import best51 from '../img/catal-2-1x-min.jpg';
 import best52 from '../img/catal-2-2x-min.jpg';
 import best61 from '../img/catal-3-1x-min.jpg';
 import best62 from '../img/catal-3-2x-min.jpg';
+// -------------------------------------------------//
+
+import choux1 from '../img/shy-min.jpg';
+import choux2 from '../img/shy2-min.jpg';
+import choux3 from '../img/shy3-min.jpg';
+import choux4 from '../img/shy4-min.jpg';
+import choux5 from '../img/best-3-1x-min.jpg';
+import choux6 from '../img/tistechko4.png';
+
+// -----------------------------------------------//
+
+import tistechko1 from '../img/tistechko7-min.jpg';
+import tistechko2 from '../img/tistechko6-min.jpg';
+import tistechko3 from '../img/tistechko.png';
+import tistechko4 from '../img/tistechko2.png';
+import tistechko5 from '../img/tistechko3.png';
+import tistechko6 from '../img/tistechko5.png';
+
+// ------------------------------------------------//
+
+import pizza1 from '../img/pizza1-min.jpeg';
+import pizza2 from '../img/pizza2-min.jpg';
+import pizza3 from '../img/pizza3-min.jpg'; 
+import pizza4 from '../img/pizza4-min.jpeg';
+import pizza5 from '../img/pizza6-min.jpg';
+import pizza6 from '../img/pizza7-min.jpg';
+
+// ---------------------------PIZZA--------------------------------------//
+
+const responsePizza = [
+    {
+    src : pizza1,
+    title : "Pizza 'Cezar'",
+    price : "300UAH/1",
+    },
+    {
+        src : pizza2,
+        title : "Pizza '4-meat'",
+        price : "400UAH/1",
+     },
+     {
+        src : pizza3,
+        title : "Pizza '4-chesse'",
+        price : "350UAH/1",
+     },
+     {
+        src : pizza4,
+        title : "Pizza '4-seasons'",
+        price : "400UAH/1",
+     },
+     {
+        src : pizza5,
+        title : "Hawaiian pizza",
+        price : "300UAH/1",
+     },
+     {
+        src : pizza6,
+        title : "Viennese pizza",
+        price : "300UAH/1",
+     },
+];
+
+//------------------------CATALOG CHOUX PASTRY-------------------------------//
+
+const responseChouxPastry = [
+    {
+      src : choux1,
+      title : "Classic cream puff",
+      price : "200UAH/5pcs",
+    },
+     {
+        src : choux2,
+        title : "Cream puff with strawberry filling",
+        price : "300UAH/5pcs",
+      },
+      {
+        src : choux3,
+        title : "Cream puff with strawberry filling and chocolate topping",
+        price : "500UAH/5pcs",
+      },
+      {
+        src : choux4,
+        title : "French-style cream puff",
+        price : "500UAH/5pcs",
+      },
+      {
+        src : choux5,
+        title : "Choux pastry",
+        price : "400UAH/4pcs",
+    },
+    {
+        src : choux6,
+        title : "Cream puff with nuts",
+        price : "600UAH/4pcs",
+    },
+]
+
+// -----------------------------BEST SELLERS RESPONSE ----------------------------------//
 
 const responseBestSellers = [
     {
@@ -23,12 +120,6 @@ const responseBestSellers = [
         srcset : best22,
         title : "Chocolate brownie",
         price : "300UAH/3pcs",
-    },
-    {
-        src : best31,
-        srcset : best32,
-        title : "Choux pastry",
-        price : "400UAH/4pcs",
     },
     {
         src : best41,
@@ -48,6 +139,48 @@ const responseBestSellers = [
         title : "Snickers cake",
         price : "1400UAH/3kg",
     },
+    {
+        src : best31,
+        srcset : best31,
+        title : "Napoleon",
+        price : "700UAH/2kg",
+    },
 ]
 
-export {responseBestSellers};
+
+// --------------------------------------------TISTECHKO----------------------------------//
+
+const responseTistechko = [
+    {
+        src : tistechko1,
+        title : "Pistachio-raspberry pastry",
+        price : "100UAH/1pcs",
+    }, 
+    {
+        src : tistechko2,
+        title : "Waffle-nut pastry",
+        price : "350UAH/2pcs",
+    },
+    {
+        src : tistechko3,
+        title : "Strawberry tart",
+        price : "900UAH/5pcs",
+    },
+    {
+        src : tistechko4,
+        title : "Ecstasy pastry",
+        price : "600UAH/3pcs",
+    },
+    {
+        src : tistechko5,
+        title : "Pastry with custard cream and strawberries",
+        price : "600UAH/6pcs",
+    },
+    {
+        src : tistechko6,
+        title : "Berry cheesecake",
+        price : "500UAH/3pcs",
+    },
+]
+
+export {responseBestSellers , responseChouxPastry, responseTistechko, responsePizza};
--- src/js/catalog.js
+++ src/js/catalog.js
@@ -1,11 +1,64 @@
-// import Swiper from 'swiper/bundle';
-// import 'swiper/bundle.min.css';
-
 import Swiper from 'swiper';
-if (window.innerWidth<600){
+import {
+  responseChouxPastry,
+  responseTistechko,
+  responsePizza,
+  responseBestSellers,
+} from './cakesApi';
 
-new Swiper ('.swiper', {
-        spaceBetween: 16,
-        slidesPerView: 'auto',
-})
+if (window.innerWidth < 600) {
+  new Swiper('.swiper', {
+    spaceBetween: 16,
+    slidesPerView: 'auto',
+  });
+}
+
+const refs = {
+  catalList: document.querySelector('.catalog-list'),
+  catalBtns: document.querySelectorAll('.catalog-btn'),
+  swiperWrapper: document.querySelector('.swiper-wrapper'),
+};
+
+const dataArray = [
+  responseBestSellers,
+  responseTistechko,
+  responseChouxPastry,
+  responsePizza,
+];
+let selectedBtn = null;
+
+refs.swiperWrapper.addEventListener('click', clickOnBtn);
+refs.catalList.innerHTML = markupCatalog(dataArray[0]);
+changeBtnColor(0);
+
+function clickOnBtn(e) {
+  if (e.target.nodeName !== 'BUTTON') {
+    return;
+  }
+
+  refs.catalList.innerHTML = markupCatalog(dataArray[e.target.id]);
+  changeBtnColor(e.target.id);
+}
+
+function changeBtnColor(id) {
+  if (selectedBtn !== null) {
+    refs.catalBtns[selectedBtn].classList.remove('js-btn-color');
+  }
+  refs.catalBtns[id].classList.add('js-btn-color');
+  selectedBtn = id;
+}
+
+function markupCatalog(data) {
+  return data
+    .map(el => {
+      return `<li class="catal-item">
+        <img src="${el.src}" alt="cakes" loading ="lazy" class="catalog-img fade-in">
+        <div class="catal-wrap">
+        <h4 class="catal-list-title">${el.title}</h4>
+        <p class="catal-list-p">${el.price}</p>
+        <svg class="catal-shop-card"><use href="../img/svg.svg#icon-shoping-cart"></use></svg>
+        </div>
+        </li>`;
+    })
+    .join(' ');
 }
